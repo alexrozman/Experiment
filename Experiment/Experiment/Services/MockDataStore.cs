6447bd9e32730a5290c4c821334da26fae2a7ccf
alexrozman <32561449+alexrozman@users.noreply.github.com>
2017-11-25 11:15:41 +1100

    First change

--- Experiment/Experiment/Services/MockDataStore.cs
+++ Experiment/Experiment/Services/MockDataStore.cs
@@ -21,6 +21,7 @@ namespace Experiment
                 new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
                 new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
                 new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "Seventh item", Description="This is an item description." },
             };
 
             foreach (var item in mockItems)
